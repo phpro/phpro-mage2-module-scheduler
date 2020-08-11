447a63d033834e8b079b8684a096671697dfc85d
Hans Vereyken <hans.vereyken@phpro.be>
2020-08-11 12:23:44 +0200

    CM-853 fix code styling

--- Block/Adminhtml/Timeline/Index.php
+++ Block/Adminhtml/Timeline/Index.php
@@ -120,11 +120,7 @@ class Index extends Template
     private function configureStartTime($date): int
     {
         if ($this->config->getTimelineLimit() === TimelineLimit::LIMIT_0) {
-            return $this->converter->toHourTimestamp(
-               null === $date
-                    ? 'now'
-                    : $date
-            );
+            return $this->converter->toHourTimestamp(null === $date ? 'now' : $date);
         }
 
         return ($this->converter->toHourTimestamp('now') - ($this->config->getTimelineLimit() * 3600));
