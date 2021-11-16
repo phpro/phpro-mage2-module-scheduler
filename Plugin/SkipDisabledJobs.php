e3d2f8d08e47258a74101377b843565018011646
Thomas Brankaer <thomas.brankaer@induxx.be>
2021-11-16 14:55:49 +0100

    BELBO2-1121 - added extra check manually scheduled jobs

--- Plugin/SkipDisabledJobs.php
+++ Plugin/SkipDisabledJobs.php
@@ -6,27 +6,36 @@ namespace Phpro\Scheduler\Plugin;
 
 use Phpro\Scheduler\Model\JobRepository;
 use Magento\Cron\Model\Schedule;
+use Magento\Cron\Model\ScheduleFactory;
 
 class SkipDisabledJobs
 {
-    /**
-     * @var JobRepository
-     */
-    private $jobRepository;
+    private JobRepository $jobRepository;
+    private ScheduleFactory $scheduleFactory;
 
-    public function __construct(JobRepository $jobRepository)
-    {
+    public function __construct(
+        JobRepository $jobRepository,
+        ScheduleFactory $scheduleFactory
+    ) {
         $this->jobRepository = $jobRepository;
+        $this->scheduleFactory = $scheduleFactory;
     }
 
     public function afterTrySchedule(Schedule $subject, bool $result): bool
     {
-        if ($subject->getScheduledManually()) {
-            return true;
-        }
-
         if (!$this->jobRepository->isEnabled($subject->getJobCode())) {
             return false;
+        }
+
+        // check if we find the a schedule with the same scheduled_at and the scheduled_manually active
+        // if so, we return true
+        $pendingJobs = $this->scheduleFactory->create()->getCollection();
+        $pendingJobs->addFieldToFilter('status', Schedule::STATUS_PENDING);
+        $pendingJobs->addFieldToFilter('job_code', ['eq' => $subject->getJobCode()]);
+        $pendingJobs->addFieldToFilter('scheduled_manually', 1);
+
+        if ($pendingJobs->count()) {
+            return true;
         }
 
         return $result;
